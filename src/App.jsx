6cb501e1a7675d1b02f57fcef790eeb6f402b2d3
Natalia Dirube <natalia.dirube@go-scm.com>
2026-03-31 13:24:01 -0300

    primer commit: app base

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>Resource Planning App</h1>
+      <p>Bienvenido. Esta es mi primera app React.</p>
+    </div>
+  );
+}
+
+export default App;
+
