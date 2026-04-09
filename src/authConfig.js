b59e83a8b1b103a5025b5766a0aa60c358f092e2
Natalia Dirube <natalia.dirube@go-scm.com>
2026-04-09 19:37:10 -0300

    fix redirectUri

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "24283d96-b5f1-4161-9163-39d685602303",
     authority: "https://login.microsoftonline.com/a23aa37c-3cbc-4c48-b731-1dd54489367a",
-    redirectUri: "http://localhost:5173",
+    redirectUri: window.location.origin,
   },
 };
 
